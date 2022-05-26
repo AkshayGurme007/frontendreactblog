2281326a9e4bb023160fa845d0818ddff42e3bde
AkshayGurme007 <gurmeakshay200@gmail.com>
2022-05-27 05:21:57 +0530

    first react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Bollywood from "./components/Bollywood";
+import Fitness from "./components/Fitness";
+import Head from "./components/Head";
+import Api from "./components/Api";
+import Fapi from "./components/Fapi";
+import Food from "./components/Food";
+import Home from "./components/Home";
+import Torisum from "./components/Torisum";
+import Fooapi from "./components/Fooapi";
+import Torapi from "./components/Torapi";
+import Latestapi from "./components/Latestapi";
+import Topapi from "./components/Topapi";
+import Articalapi from "./components/Articalapi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Topapi>
+      <Articalapi>
+        <Latestapi>
+          <Torapi>
+            <Fooapi>
+              <Fapi>
+                <Api>
+                  <>
+                    <BrowserRouter>
+                      <Head />
+                      <Routes>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/torisum" element={<Torisum />} />
+                        <Route path="/bollywood" element={<Bollywood />} />
+                        <Route path="/fitness" element={<Fitness />} />
+                        <Route path="/food" element={<Food />} />
+                      </Routes>
+                    </BrowserRouter>
+                  </>
+                </Api>
+              </Fapi>
+            </Fooapi>
+          </Torapi>
+        </Latestapi>
+      </Articalapi>
+    </Topapi>
   );
 }
 
